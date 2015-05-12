d70af477b2bedca32cbb071d6a64d2e818e71c00
min.kyoungkook <min.kyoungkook@cashslide.co.kr>
2015-05-12 19:11:03 +0900

    무지하게 올래걸리는 버전

--- lib/last_prime_number.rb
+++ lib/last_prime_number.rb
@@ -8,6 +8,26 @@ class LastPrimeNumber
  end
 
  def big_prime(n)
-   3
+ 	i = n
+ 	p_array = []
+
+ 	for p in 2..n
+ 		if is_prime_number(p)
+ 			while i % p == 0
+ 				p_array << p
+ 				i = i / p
+ 			end
+ 		end
+ 	end
+
+ 	p_array.last
+ end
+
+ def is_prime_number(n)
+ 	return false if 2 > n
+
+ 	for p in 2..n
+ 		return p == n if n % p == 0	
+ 	end
  end
 end
--- spec/last_prime_number_spec.rb
+++ spec/last_prime_number_spec.rb
@@ -13,7 +13,43 @@ RSpec.describe "마지막소수" do
       expect(lpn.big_prime(6)).to eq(3)
     end
 
-    
+    it '1013의 가장큰 소수?' do
+      lpn = LastPrimeNumber.new
+      expect(lpn.big_prime(1013)).to eq(1013)
+    end
 
+    it '600851475143의 가장큰 소수?' do
+      lpn = LastPrimeNumber.new
+      expect(lpn.big_prime(600851475143)).to eq(1013)
+    end
+
+    
   end
+
+  describe "#소수인지 확인하기" do
+    it '1 ?' do
+      lpn = LastPrimeNumber.new
+      expect(lpn.is_prime_number(1)).to eq(false)
+    end
+
+    it '2 ?' do
+      lpn = LastPrimeNumber.new
+      expect(lpn.is_prime_number(2)).to eq(true)
+    end
+
+    it '3 ?' do
+      lpn = LastPrimeNumber.new
+      expect(lpn.is_prime_number(3)).to eq(true)
+    end
+
+    it '4 ?' do
+      lpn = LastPrimeNumber.new
+      expect(lpn.is_prime_number(4)).to eq(false)
+    end
+
+    it '1013 ?' do
+      lpn = LastPrimeNumber.new
+      expect(lpn.is_prime_number(1013)).to eq(true)
+    end    
+   end
 end
